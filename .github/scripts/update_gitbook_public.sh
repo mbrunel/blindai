646d615dd475e526a149a29a202b7b29773c0915
Matthias Brunel <matthias.brunel@mithrilsecurity.io>
2022-06-22 19:35:51 +0200

    Test

--- .github/scripts/update_gitbook_public.sh
+++ .github/scripts/update_gitbook_public.sh
@@ -4,7 +4,7 @@ set -x
 git config --global user.email "matthias.brunel@mithrilsecurity.io"
 git config --global user.name "mbrunel"
 
-git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@mbrunel/gitbook_public.git" "./gitbook"
+git clone --single-branch --branch master "https://x-access-token:$API_TOKEN_GITHUB@github.com/mbrunel/gitbook_public.git" "./gitbook"
 
 cp "docs/client.py.md" "gitbook/resources/client-api-reference/client-interface-$VERSION.md"
 cd gitbook
